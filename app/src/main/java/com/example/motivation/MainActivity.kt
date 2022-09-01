e92ce7e7d32b8f135bccbdfeb764227e4b2ab3c4
Patrick Rafael <Patrickrafael05@gmail.com>
2022-09-01 18:12:02 -0300

    Interface

---
+++ app/src/main/java/com/example/motivation/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.motivation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.motivation.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Hide ActionBar
+        supportActionBar?.hide()
+
+
+        //CLick event
+        binding.buttonNewPhrase.setOnClickListener { this }
+
+    }
+
+    override fun onClick(view: View) {
+
+        if(view.id == R.id.button_new_phrase){
+
+
+
+        }
+
+    }
+}
